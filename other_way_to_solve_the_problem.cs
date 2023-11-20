ab9d5d8e9be086e60c8d5b48ce77811e26e0ed06
angelestupinan <angel200@nauta.cu>
2023-11-19 22:47:15 -0500

    added the lists and the function to add items

--- other_way_to_solve_the_problem.cs
+++ other_way_to_solve_the_problem.cs
@@ -1,7 +1,22 @@
 namespace AirportAdministrator{
     class Program{
         static void Main(string[] args){
-            
+            List<string> flight_destination = new List<string>();
+            List<string> flight_code = new List<string>();
+            List<string> flight_number = new List<string>();
+            List<string> flight_seats = new List<string>();
+
+            public void addFlight(string a, string b, string c, string d){
+                if(a!=" "&&b!=" "&&c!=" "&&d!=" "){
+                  flight_destination.Add(a.ToUpper());
+                  flight_code.Add(b.ToUpper());
+                  flight_number.Add(c.ToUpper());
+                  flight_seats.Add(d.ToUpper());  
+                }
+                else{
+                    Console.Writeline("ninguno de los valores puede estar vacio"); 
+                }
+            }
         }
     }
 }
